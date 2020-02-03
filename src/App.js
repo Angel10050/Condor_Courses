2d5a0a93973650c65a921a839430f9371ac72935
angel10050 <angelangel10050@gmail.com>
2020-02-03 15:51:22 -0500

    restore proxy

--- src/App.js
+++ src/App.js
@@ -50,10 +50,7 @@ class App extends Component {
     });
 
     try {
-      const query = await axios.get(
-        `https://test.mytablemesa.com${this.state.url}`,
-        { headers: { "Access-Control-Allow-Origin": "*" } }
-      );
+      const query = await axios.get(this.state.url);
       this.setState({
         loading: false,
         url: query.data.next,
@@ -73,8 +70,7 @@ class App extends Component {
   HandlerFilter = async () => {
     try {
       const filtredQuery = await axios.get(
-        `https://test.mytablemesa.com/api/courses?orderBy=popularity+desc&expand=provider&name=${this.state.courseName}`,
-        { headers: { "Access-Control-Allow-Origin": "*" } }
+        `/api/courses?orderBy=popularity+desc&expand=provider&name=${this.state.courseName}`
       );
       this.setState({
         filterInfo: {
